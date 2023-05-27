8617b91fc43aeed539edaa493602e55dbff4a319
Vincent Mayer <vincent.mayer.1808@gmail.com>
2023-05-27 18:43:34 -0400

    request, formatdata

---
+++ assets/js/createGraphPrec.js
@@ -0,0 +1,20 @@
+
+export let createGraphPrec;
+
+createGraphPrec = (canvas, data) => {
+  const chart = new Chart(canvas, {
+    type: 'bar',
+    data: {
+      labels: data.time,
+      datasets: [
+        {
+          label: 'Precipitation',
+          data: data.precipitation,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    },
+  });
+};
