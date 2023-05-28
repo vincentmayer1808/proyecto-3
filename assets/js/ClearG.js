57000b971539a0e48c3bef37c3752255a26f4301
Victor Briones <vbrionesparra@gmail.com>
2023-05-28 17:14:17 -0400

    ClearG fix varchart to chartArray

--- assets/js/ClearG.js
+++ assets/js/ClearG.js
@@ -1,11 +1,31 @@
 export const ClearG=(chart)=>{
-    try {
-        
-        if(chart){
-            chart.destroy();
+    // try {      
+    //     if(chart){
+    //         chart.destroy();
 
+    //     }
+    // } catch (error) {
+    //    console.log("error chartArray"); 
+    // }
+
+    try {
+        if(chart){
+            chart.forEach(element => {
+                console.log(element.canvas);
+                element.destroy();  
+                console.log(element.canvas);
+                 
+            });
+            chart.splice(0,chart.length);
+            
+        }else{
+            console.log("array vacio");
         }
     } catch (error) {
-       console.log("error chartArray"); 
+        console.log(error)
     }
+
+
+
+
 }
