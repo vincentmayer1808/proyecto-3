b4f32a967027eddd5faa25a76338ff88c67c4016
Victor Briones <vbrionesparra@gmail.com>
2023-05-29 20:14:37 -0400

    error input with sweetalert

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,11 +1,10 @@
 export const makeRequest = async (input) => {
   const latLonData = await makeLatLonRequest(input);
   const { longitude, latitude } = latLonData;
-  console.log(longitude);
   const endpoint = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=auto`;
   const output = await axios.get(endpoint);
   try {
-    console.log(output);
+    
     if (output.status === 200) {
       const datos = output.data;
       return datos;
@@ -16,21 +15,30 @@ export const makeRequest = async (input) => {
 };
 
 export const makeLatLonRequest = async (input) => {
-  console.log("makeLatLonRequest");
+  
   console.log(input.value);
   const endpoint = `https://geocoding-api.open-meteo.com/v1/search?name=${input.value}&count=10&language=es&format=json`;
   const output = await axios.get(endpoint);
+  console.log(output.data.results);
 
   try {
-    console.log(output);
+    
     if (output.status === 200) {
-      console.log(output.data.results[0]);
-      const datos = output.data.results[0];
-      return datos;
+      
+        const datos = output.data.results[0];
+        return datos;
+      
+      
     } else {
-      console.log("Error");
+      console.log("Error status");
     }
   } catch (error) {
-    throw new error();
+    Swal.fire({
+      icon: 'error',
+      title: '404',
+      text: 'Nombre de ciudad no existe!',
+      
+    })
+   
   }
 };
